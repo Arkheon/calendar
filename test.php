970cd6b54820cb7e1d34a0ab31e8cb26aef74caf
Arkheon <vincent.mamelin@gmail.com>
2016-05-31 18:14:55 +0200

    Import dates gestassa xml

--- test.php
+++ test.php
@@ -1,90 +1,50 @@
-
-
-
-<?php 
-
-
-/* balise exemple
-<event>
-		<id>5</id>
-		<name>Here's an Event</name>
-		<startdate>2016-3-10</startdate>
-		<enddate>2016-3-12</enddate>
-		<starttime>12:00</starttime>
-		<endtime>2:00</endtime>
-		<color>#ffb128</color>
-		<url></url>
-</event>*/
-
-
+<?php
+ 
+$file= fopen( "toto.xml", "r+" );
+fwrite($file, '<?xml version="1.0" encoding="UTF-8"?>');
+fwrite($file,"<monthly>");
 
 date_default_timezone_set('Europe/Brussels');
 $year=date("Y");
-// boucle  sur les jours de l'année
-$id=0;
-for( $i = 1; $i <= 365; $i++ ) 
-{ 
-if ( date("l", mktime(0, 0, 0, 1, $i, $year)) == 'Thursday')
+
+$mesdates = simplexml_load_file('Export.xml');
+/*echo $mesdates->MaTABLE[1]->Date_Limite;*/
+
+foreach ($mesdates as $dateinfo):
+        $dateech=$dateinfo->DatEch;
+		$datelim=$dateinfo->Date_Limite;
+		
+		/*echo "date echeance du " .$dateech." le traitement aura lieu le ".$datelim."</br>";*/
+		
+		$p1 = explode("/",$dateech);
+		$p2 = explode("/",$datelim);
+		
+		
+			if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "01")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Arrerages</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "06")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Prelevement 6</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "20")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Prelevement 20</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		
+		
+    endforeach;
 	
-echo "<event><id>".++$id."</id><name>Relances</name><startdate>".date("Y-m-d", mktime(0, 0, 0, 1, $i, $year)). "</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>";  
-
-}
-
-for( $j = 1; $j <= 365; $j++ ) { 
-   if (date("l", mktime(0, 0, 0, 1, $j, $year)) == 'Monday')
-	   
-   /*echo date("Y-m-d", mktime(0, 0, 0, 1, $j, $year)) ." Suivi de paiement". "<br />"; */
-   echo "<event><id>".++$id."</id><name>Suivi de paiement</name><startdate>".date("Y-m-d", mktime(0, 0, 0, 1, $j, $year)). "</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>";  
- }
- 
- 
- 
- for( $k = 1; $k <= 12; $k++ ) { 
- if ($k < 10)   
- {   
- echo "<event><id>".++$id."</id><name>Reception fonds</name><startdate>".$year."-0".$k."-20</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }
-
- else {
-echo "<event><id>".++$id."</id><name>Reception fonds</name><startdate>".$year."-".$k."-20</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }	 
-
- }
-
-  for( $l = 1; $l <= 12; $l++ ) { 
- if ($l < 10)   
- {   
- echo "<event><id>".++$id."</id><name>Reception fonds</name><startdate>".$year."-0".$l."-06</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }
-
- else {
-echo "<event><id>".++$id."</id><name>Reception fonds</name><startdate>".$year."-".$l."-06</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }	 
-
- }
- 
- 
-
- 
- 
- 
-
-for( $m = 1; $m <= 12; $m++ ) { 
- if ($m < 10)   
- {   
-$d = new DateTime( $year."-0".$m );{
-echo "<event><id>".++$id."</id><name>FDM</name><startdate>".$d->format( 'Y-m-t' )."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }
-}
- else {
-$d = new DateTime( $year."-".$m);
-echo "<event><id>".++$id."</id><name>FDM</name><startdate>".$d->format( 'Y-m-t' )."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#e67e22</color></event>";  }
-	 }
- 
- for( $n = 1; $n <= 12; $n++ ) { 
- if ($n < 10)   
- {   
- echo "<event><id>".++$id."</id><name>Controles FDM</name><startdate>".$year."-0".$n."-25</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#3498db</color></event>";  }
-
- else {
-echo "<event><id>".++$id."</id><name>Controles FDM</name><startdate>".$year."-".$n."-25</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#3498db</color></event>";  }	 
- }
-
-
-
-?> 
+fwrite($file,"</monthly>");
+fclose($file);
+?>
--- write.php
+++ write.php
@@ -70,6 +70,45 @@ fwrite($file,"<event><id>".++$id."</id><name>Reboot SQL</name><startdate>".$year
 fwrite($file,"<event><id>".++$id."</id><name>Reboot SQL</name><startdate>".$year."-".$n."-22</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#9b59b6</color></event>");  }	 
  }
  
+ /*IMPORT DATES PRLVTS ISSUE XML GESTASSA*/
+ 
+ 
+ $mesdates = simplexml_load_file('Export.xml');
+ 
+ foreach ($mesdates as $dateinfo):
+        $dateech=$dateinfo->DatEch;
+		$datelim=$dateinfo->Date_Limite;
+ 
+		$p1 = explode("/",$dateech);
+		$p2 = explode("/",$datelim);
+ 
+ if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "01")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Arrerages</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "06")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Prelevement 6</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		if ($p1[2] == $year AND $p2[2] == $year AND $p1[0] == "20")
+		{
+			/*echo $p1[2]."/". $p1[1]."/". $p1[0]."</br>";*/
+			
+			fwrite($file,"<event><id>".++$id."</id><name>Prelevement 20</name><startdate>".$p2[2]."/". $p2[1]."/". $p2[0]."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#2ecc71</color></event>"); 
+		}
+		
+		
+		
+    endforeach;
+ 
+
+ 
 fwrite($file,"</monthly>");
 fclose($file);
 
