50db5b0bfbe2f62d74d537b0af03876c4b7cc2c5
Arkheon <vincent.mamelin@gmail.com>
2016-05-31 22:17:34 +0200

    Ferie

--- write.php
+++ write.php
@@ -9,6 +9,42 @@ date_default_timezone_set('Europe/Brussels');
 $year=date("Y");
 $id=0;
 
+function getHolidays($year = null)
+{
+  if ($year === null)
+  {
+    $year = intval(date('Y'));
+  }
+ 
+  $easterDate  = easter_date($year);
+  $easterDay   = date('j', $easterDate);
+  $easterMonth = date('n', $easterDate);
+  $easterYear   = date('Y', $easterDate);
+ 
+  $holidays = array(
+    // Dates fixes
+   date("Y-m-d", mktime(0, 0, 0, 1,  1,  $year)),  // 1er janvier
+   date("Y-m-d", mktime(0, 0, 0, 5,  1,  $year)),  // Fête du travail
+   date("Y-m-d", mktime(0, 0, 0, 5,  8,  $year)),  // Victoire des alliés
+   date("Y-m-d", mktime(0, 0, 0, 7,  14, $year)),  // Fête nationale
+   date("Y-m-d", mktime(0, 0, 0, 8,  15, $year)),  // Assomption
+   date("Y-m-d", mktime(0, 0, 0, 11, 1,  $year)),  // Toussaint
+   date("Y-m-d", mktime(0, 0, 0, 11, 11, $year)),  // Armistice
+   date("Y-m-d", mktime(0, 0, 0, 12, 25, $year)),  // Noel
+ 
+    // Dates variables
+   date("Y-m-d", mktime(0, 0, 0, $easterMonth, $easterDay - 2,  $easterYear)),
+   date("Y-m-d", mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear)),
+   date("Y-m-d", mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear)),
+   date("Y-m-d", mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear)),
+  );
+ 
+  sort($holidays);
+ 
+  return $holidays;
+   
+}
+
 for( $i = 1; $i <= 365; $i++ ) 
 { 
 if ( date("l", mktime(0, 0, 0, 1, $i, $year)) == 'Thursday')
@@ -108,7 +144,10 @@ fwrite($file,"<event><id>".++$id."</id><name>Reboot SQL</name><startdate>".$year
 		
     endforeach;
  
-
+$ferie=getHolidays ($year);
+foreach ($ferie as $jferie) {
+	fwrite($file,"<event><id>".++$id."</id><name>Ferie</name><startdate>".$jferie."</startdate><enddate></enddate><starttime></starttime><endtime></endtime><color>#354b60</color></event>"); 
+}
  
 fwrite($file,"</monthly>");
 fclose($file);
